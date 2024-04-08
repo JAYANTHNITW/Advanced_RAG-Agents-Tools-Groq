fe07c1216188ffa32ec2c536158fc468e192d06b
JAYANTHNITW <jayanthnitw@gmail.com>
2024-04-08 20:05:30 +0530

    done

---
+++ localaama.py
@@ -0,0 +1,37 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+from langchain_community.llms import ollama
+
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ["LANGCHAIN_TRACKING_V2"] = "true" # for langchain tracking
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY") 
+
+## Prompt Template
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "you are a helpful assistant. Please response to the user queries"),
+        ("user","Question:{question}")
+    ]
+)
+
+## streamlit framework
+
+st.title("Langchain Demo With OPENAI API")
+input_text = st.text_input("Search the topic u want")
+
+# ollama Llama2 LLM
+
+llm = ollama(model="llama2")
+output_parser = StrOutputParser()
+chain = prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({'question':input_text}))
